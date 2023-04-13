8d3eee23f57d08c70185ab5be2f9d45f73f7bd14
MoyaEgun <kyu4483@gmail.com>
2023-04-13 21:11:39 +0900

    0413

--- src/main/java/com/kh/yagiyo/domain/entity/Board.java
+++ src/main/java/com/kh/yagiyo/domain/entity/Board.java
@@ -3,19 +3,25 @@ package com.kh.yagiyo.domain.entity;
 import com.kh.yagiyo.web.form.board.BoardForm;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // DB의 테이블 역활을 하는 클래스
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "board")
 @SequenceGenerator(
-        name = "BOARD_ID_SEQ",
-        sequenceName = "BOARD_ID_SEQ",
+        name = "ID_SEQ",
+        sequenceName = "ID_SEQ",
         initialValue = 1, allocationSize = 1)
 public class Board extends Base{
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_ID_SEQ")
-  private Long BOARD_ID;
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_SEQ")
+  private Long ID;
 
   @Column(length = 20, nullable = false) // 크기 20. not null
   private String BOARD_WRITER;
@@ -32,6 +38,12 @@ public class Board extends Base{
   @Column
   private int BOARD_HITS;
 
+  @Column
+  private int FILEATTACHED; // 1 or 0
+
+  @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE,orphanRemoval = true,fetch = FetchType.LAZY)
+  private List<BoardFileEntity> boardFileEntityList = new ArrayList<>();
+
   public static Board toSaveEntity(BoardForm boardForm){
     Board board = new Board();
     board.setBOARD_WRITER(boardForm.getBoardWriter());
@@ -39,7 +51,29 @@ public class Board extends Base{
     board.setBOARD_TITLE(boardForm.getBoardTitle());
     board.setBOARD_CONTENTS(boardForm.getBoardContents());
     board.setBOARD_HITS(0);
+    board.setFILEATTACHED(0); // 파일 없음.
     return board;
+  }
 
+  public static Board toUpdateEntity(BoardForm boardForm) {
+    Board board = new Board();
+    board.setID(boardForm.getId());
+    board.setBOARD_WRITER(boardForm.getBoardWriter());
+    board.setBOARD_PASS(boardForm.getBoardPass());
+    board.setBOARD_TITLE(boardForm.getBoardTitle());
+    board.setBOARD_CONTENTS(boardForm.getBoardContents());
+    board.setBOARD_HITS(boardForm.getBoardHits());
+    return board;
+  }
+
+  public static Board toSaveFileEntity(BoardForm boardForm) {
+    Board board = new Board();
+    board.setBOARD_WRITER(boardForm.getBoardWriter());
+    board.setBOARD_PASS(boardForm.getBoardPass());
+    board.setBOARD_TITLE(boardForm.getBoardTitle());
+    board.setBOARD_CONTENTS(boardForm.getBoardContents());
+    board.setBOARD_HITS(0);
+    board.setFILEATTACHED(1); // 파일 있음.
+    return board;
   }
 }
--- src/main/java/com/kh/yagiyo/web/BoardController.java
+++ src/main/java/com/kh/yagiyo/web/BoardController.java
@@ -5,10 +5,14 @@ import com.kh.yagiyo.domain.board.svc.boardSVC;
 import com.kh.yagiyo.web.form.board.BoardForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -26,7 +30,7 @@ private final boardSVC boardSVC;
     return "/board/boardSave";
   }
   @PostMapping("/save")
-  public String save(@ModelAttribute BoardForm boardForm){
+  public String save(@ModelAttribute BoardForm boardForm)throws IOException{
     System.out.println("boardForm = " + boardForm);
     boardSVC.save(boardForm);
 
@@ -37,5 +41,57 @@ private final boardSVC boardSVC;
     List<BoardForm> boardList = boardSVC.findAll();
     model.addAttribute("boardList", boardList);
     return "/board/boardList";
+  }
+  @GetMapping("/{id}")
+  public String findById(@PathVariable Long id,Model model,
+                          @PageableDefault(page=1) Pageable pageable){
+//    해당 게시글의 조회수를 하나 올리고
+//    게시글 데이터를 가져와서 detail.html에 출력
+    boardSVC.updateHits(id);
+    BoardForm boardForm = boardSVC.findById(id);
+    model.addAttribute("board", boardForm);
+    model.addAttribute("page", pageable.getPageNumber());
+    return "/board/boardDetail";
+  }
+  @GetMapping("/update/{id}")
+  public String updateForm(@PathVariable Long id,Model model) {
+    BoardForm boardForm = boardSVC.findById(id);
+    model.addAttribute("board", boardForm);
+    return "/board/boardUpdate";
+  }
+  @PostMapping("/update/{id}")
+  public String update(@ModelAttribute BoardForm boardForm,Model model) {
+    BoardForm board = boardSVC.update(boardForm);
+    model.addAttribute("board", board);
+    return "/board/boardDetail";
+//    return "redirect:/board/" + boardForm.getId();
+  }
+  @GetMapping("/delete/{id}")
+  public String delete(@PathVariable Long id){
+    boardSVC.delete(id);
+    return "redirect:/board";
+  }
+
+  // /board/paging?page=1
+  @GetMapping("/paging")
+  public String Paging(@PageableDefault(page = 1)Pageable pageable,Model model){
+//    pageable.getPageNumber();
+    Page<BoardForm> boardList = boardSVC.paging(pageable);
+    int blockLimit = 3;
+    int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+    int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+
+    //page 갯수 20개
+    // 현재 사용자가 3페이지
+    // 1 2 3
+    // 보여지는 페이지 갯수 3개
+    // 총 페이지 갯수 8개
+
+    model.addAttribute("boardList", boardList);
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
+    return "/board/paging";
+
   }
 }
